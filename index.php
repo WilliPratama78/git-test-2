0ff1fadbccdeda8f39bb76ecbe05d15c916283ef
Willi Pratama <pratamawilly85@gmail.com>
2025-04-07 15:16:33 +0700

    Menambahkan paragraph

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, incidunt eveniet. Molestias, cum laboriosam distinctio voluptatibus sit fugit dignissimos saepe illum sequi velit quae nisi vitae consectetur excepturi neque cupiditate!</p>
 </body>
 
 </html>
